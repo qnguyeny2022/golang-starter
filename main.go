81de77fb70d1e62b41da0e944173fac666c6c2c4
qnguyeny2022 <90001012+qnguyeny2022@users.noreply.github.com>
2021-09-29 19:35:37 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+"fmt"
+"os"
+"strconv"
+)
 
 func main() {
-	fmt.Println("नमस्ते, 世界.")
+input := os.Args
+if len(input) == 2 {
+input := os.Args[1]
+num, err := strconv.Atoi(input)
+if num > 10 || num < 0 || err != nil {
+fmt.Println("ERROR!")
+} else {
+for i := 0; i < num; i++ {
+fmt.Println("friendly-trout")
+}
+}
+} else {
+fmt.Println("ERROR!")
+}
 }
